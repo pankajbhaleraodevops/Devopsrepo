fa4b33524acb38dd41515f2390c337298f45bee8
root <root@jenkin1.c.mesmerizing-bee-212303.internal>
2018-09-09 06:58:37 +0000

    java file added

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is for maven job test!" );
+        System.out.println( "This is for maven job test file!" );
     }
 }
